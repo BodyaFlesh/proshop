e9544b2b615b3ef757f278bab0bd182ab030a52f
Bohdan Sych <bodya.flesh@gmail.com>
2020-11-27 13:10:13 +0200

    seeder

--- backend/data/users.js
+++ backend/data/users.js
@@ -9,12 +9,12 @@ const users = [
     },
     {
         name: 'John Doe',
-        email: 'admin@app.com',
+        email: '1@app.com',
         password: bcrypt.hashSync('123456', 10),
     },
     {
         name: 'Jane Doe',
-        email: 'admin@app.com',
+        email: '2@app.com',
         password: bcrypt.hashSync('123456', 10),
     }
 ];
